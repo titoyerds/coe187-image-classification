7b263d1adbe9286ab30b60540948c00454ef5ed3
Andrey Fritz Solijon <161728738+titoyerds@users.noreply.github.com>
2026-01-04 00:49:44 +0800

    Added cat loaf vs not loaf training python and shell script file

---
+++ train_catsdogs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# python train.py --epochs 200 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule-catsdogs.yaml --qat-policy policies/qat_policy_cd.yaml --model ai85cdnet --dataset cats_vs_dogs --confusion --param-hist --embedding --device MAX78000 "$@"
+# python train.py --epochs 100 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule-catsdogs.yaml --qat-policy policies/qat_policy_cd.yaml --model ai85cdnet --dataset cats_vs_dogs --confusion --param-hist --embedding --device MAX78000 --compiler-mode none
+python train.py --epochs 100 --optimizer Adam --lr 0.0001 --wd 0 --deterministic --compress policies/schedule-catsdogs.yaml --qat-policy policies/qat_policy_cd.yaml --model ai85cdnet --dataset cats_vs_dogs --confusion --param-hist --embedding --device MAX78000 --compiler-mode none --enable-tensorboard
